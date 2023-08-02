a472d9beecdf8308a37232fae9195c9056531c78
Gaitho57 <michaelgaitho10@gmail.com>
2023-08-02 01:25:11 -0700

    beginning_end

--- 0x06-regular_expressions/5-beginning_and_end.rb
+++ 0x06-regular_expressions/5-beginning_and_end.rb
@@ -1,2 +1,21 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/hbt*n/).join
+#!/usr/bin/env ruby
+
+def beginning_and_end_match(input)
+  regex_pattern = /^h.n$/
+
+  match_result = input.match(regex_pattern)
+
+  !match_result.nil?
+end
+
+if ARGV.length == 1
+  input_argument = ARGV[0]
+
+  if beginning_and_end_match(input_argument)
+    puts input_argument
+  else
+    puts "$"
+  end
+end
+
