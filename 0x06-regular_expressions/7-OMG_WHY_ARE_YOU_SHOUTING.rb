2e6b8916037a01fdb68c04eba84c0db50402291e
Gaitho57 <michaelgaitho10@gmail.com>
2023-08-02 02:06:35 -0700

    OMG_WHY_ARE_YOU_SHOUTING

--- 0x06-regular_expressions/7-OMG_WHY_ARE_YOU_SHOUTING.rb
+++ 0x06-regular_expressions/7-OMG_WHY_ARE_YOU_SHOUTING.rb
@@ -1,14 +1,2 @@
 #!/usr/bin/env ruby
-
-def extract_capital_letters(input)
-  capital_letters = input.scan(/[A-Z]/)
-
-  capital_letters.join
-end
-
-if ARGV.length == 1
-  input_argument = ARGV[0]
-
-  capital_letters_result = extract_capital_letters(input_argument)
-  puts capital_letters_result
-end
+puts ARGV[0].scan(/[A-Z]/).join
