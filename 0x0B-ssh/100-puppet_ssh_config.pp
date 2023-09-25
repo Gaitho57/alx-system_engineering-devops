acfd73cf02177ab746eace0ff2536cf0cdf28e6b
Gaitho57 <michaelgaitho10@gmail.com>
2023-09-25 12:56:56 -0700

    update

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+## puppet makes changes to configuration file
+
+file {  'etc/ssh/ssh_config':
+  ensure  => present,
+
+content =>"
+  #SSH client configuration
+  host*
+  IdentifyFile ~/.ssh/school
+  passwordAuthentification no",
+}
