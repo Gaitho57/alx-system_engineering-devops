18f4ebdb8efbe02bb6c79371af5703d2bcf66ce7
Gaitho57 <michaelgaitho10@gmail.com>
2024-01-16 13:37:39 -0500

    update

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,26 +1,20 @@
 #!/usr/bin/python3
-"""Quering Reddit"""
-
+"""
+queries the number of subscribers in a subreddit
+"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """query a subreddit and retrive no of subscribers"""
+    """
+    gets the number of subscribers in the subreddit given as an arg
+    """
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {'User-Agent': 'api-test by Namasaka'}
+    res = requests.get(url, headers=headers, allow_redirects=False)
 
-    # Reddit API endpoint for getting subreddit informatiom
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-
-    # Set a custom User-Agent to avoid too many requests error
-    headers = {'User-Agent': 'My user Agent 1.0'}
-
-    # send a GET request to the Reddit API
-    response = requests.get(url, headers=headers, allow_redirects=False)
-
-    # Chek if the request was successful and not redirect
-    if response.status_code == 200:
-        # parse JSON response to extract no of subscribers
-        data = response.json().get('data', {})
-        sub_count = data.get('subscribers', 0)
-        return sub_count
-    else:
-        return 0
+    # parse if request is successful
+    if res.status_code == 200:
+        res = res.json()
+        return (res['data']['subscribers'])
+    return 0
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,28 +1,12 @@
 #!/usr/bin/python3
-"""Query subreddit"""
+"""
+1-main
+"""
+import sys
 
-import requests
-
-
-def top_ten(subreddit):
-    """Query Reddit and print titles of the first 10 hot posts"""
-
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-
-    # Set a custom User-Agent to avoid too many requests error
-    headers = {'User-Agent': 'My user Agent 1.0'}
-
-    # send a GET request to the Reddit API
-    response = requests.get(url, headers=headers, allow_redirects=False)
-
-    try:
-        response = requests.get(url, headers=headers,
-                                allow_redirects=False)
-        if response.status_code == 200:
-            children = response.json().get('data').get('children')
-            for i in range(10):
-                print(children[i].get('data').get('title'))
-        else:
-            print("None")
-    except Exception:
-        print("None")
+if __name__ == '__main__':
+    top_ten = __import__('test').top_ten
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        top_ten(sys.argv[1])
